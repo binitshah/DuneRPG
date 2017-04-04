c5797845d236afda80ad924c4890f975a8d8750d
Binit Shah <bshah@gatech.edu>
2017-04-04 01:43:22 -0400

    initial commit

---
+++ android/src/com/binitshah/dunerpg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.binitshah.dunerpg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.binitshah.dunerpg.DuneRPG;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DuneRPG(), config);
+	}
+}
---
+++ desktop/src/com/binitshah/dunerpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.binitshah.dunerpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.binitshah.dunerpg.DuneRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DuneRPG(), config);
+	}
+}
